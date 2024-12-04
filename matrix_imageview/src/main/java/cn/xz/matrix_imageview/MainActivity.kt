2cd34831f23bf5ad6c903fdae72d15511e217245
小赵小赵福星高\16354 <1635411488@>
2024-12-04 10:17:37 +0800

    初提交

---
+++ matrix_imageview/src/main/java/cn/xz/matrix_imageview/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.xz.matrix_imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
